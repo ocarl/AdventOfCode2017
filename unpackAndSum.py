d57e049fb0460a79d99017052422831e40db1bd8
Carl Onsjö <consj@caevalue.com>
2017-12-05 21:39:17 +0100

    Updated pdf and clean up

--- unpackAndSum.py
+++ unpackAndSum.py
@@ -1,7 +1,8 @@
 def compileOutput(out: "string to be added to", keys: "string to add", mySym: "integer to add") -> str:
     return out + keys[:-1] + ': ' + str(mySym) + ', '
 
-mainList = [[('a', 2), ('b', 4), ('c', 3)], [('x', 1), ('y', 2), ('z', 3)]]
+mainList = [[('a', 2), ('b', 4), ('c', 3)],
+            [('x', 1), ('y', 2), ('z', 3)]]
 
 output = ''
 
